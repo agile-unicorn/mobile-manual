d26c2a3d24063cd1b202bb40ff9065a1077ace41
Daniel Pozzi <daniel@dedica.team>
2025-06-12 17:01:20 +0200

    credits

--- src/components/manual/Credits.tsx
+++ src/components/manual/Credits.tsx
@@ -13,18 +13,19 @@ export const Credits = () => {
                     would not have been possible!
                 </section>
                 <section>
-                    Wir danken allen, die an der Entwicklung von <strong>Agile Unicorn</strong> mitgewirkt haben und uns
-                    den Rückhalt gegeben haben, um dieses Spiel zu realisieren. Ein besonderer Dank geht an unsere
-                    Familien, die uns während der Entwicklung den Rücken freigehalten haben und wirklich viel Geduld mit
-                    uns hatten.
-                    Danke auch an alle Freunde und an Testspieler, die uns
-                    wertvolles Feedback gegeben haben. Ohne euch wäre dieses Spiel nicht möglich gewesen!
+                    Wir möchten allen von Herzen danken, die an der Entstehung von <strong>Agile Unicorn</strong> mitgewirkt und uns auf
+                    unserem Weg unterstützt haben. Ein besonderer Dank gilt dabei unseren Familien: Ihr habt uns in
+                    jeder Phase den Rücken gestärkt, uns mit eurem Verständnis getragen und uns immer wieder ermutigt,
+                    an unsere Vision zu glauben! Ohne eure Unterstützung wäre dieses Spiel niemals Wirklichkeit
+                    geworden. Ebenso danken wir unseren Freunden und allen Testspieler*innen, die mit ihrem wertvollen
+                    Feedback das Spiel bereichert haben. Ihr alle habt Agile Unicorn erst möglich gemacht – vielen Dank!
                 </section>
 
                 <section>
                     <h3 className="text-xl font-semibold mb-3">Helping Hands and Special Support</h3>
                     <p className="text-gray-600 mb-2">
-                        <a href="https://www.kickstarter.com/projects/codepalm/cybersiege-computer-science-learning-card-game?lang=de" className="underline pl-1">Dennis</a>,
+                        <a href="https://www.kickstarter.com/projects/codepalm/cybersiege-computer-science-learning-card-game?lang=de"
+                           className="underline pl-1">Dennis</a>,
                         Harry K., <a href="https://jcon.one/" className="underline pl-1">JCON Team</a>,
                         <a href="https://www.parameter-b.de/de/" className="underline pl-1">Parameter B</a>
                     </p>
@@ -47,11 +48,12 @@ export const Credits = () => {
                         Julian Schmiederer,
                         knightess,
                         Lars Luz,
-                         <a href="https://www.zeit-zum-spielen.de" className="underline pl-1">Laura "Xamra" Estelmann</a>,
+                        <a href="https://www.zeit-zum-spielen.de" className="underline pl-1">Laura "Xamra" Estelmann</a>,
                         Linus A. (Münster),
-                        Matthias Schrammek, 
+                        Matthias Schrammek,
                         Maximilian Gernandt,
-                         <a href="https://www.linkedin.com/in/michael-c-hanson-" className="underline pl-1">Michael Hanson</a>,
+                        <a href="https://www.linkedin.com/in/michael-c-hanson-" className="underline pl-1">Michael
+                            Hanson</a>,
                         Mikedaquick,
                         Philipp Hochwald (Bonn),
                         Ralf Teusner,
@@ -59,8 +61,10 @@ export const Credits = () => {
                         Sally Mackrodt,
                         Stefan Gatner,
                         Stephan (Mönchengladbach),
-                        <a href="https://www.linkedin.com/in/stephanie-becker-a5a65a248" className="underline pl-1">Stephanie (Frankfurt)</a>
-                         <a href="https://www.linkedin.com/in/thorsten-thamm" className="underline pl-1">Thorsten Thamm (Köln)</a>,
+                        <a href="https://www.linkedin.com/in/stephanie-becker-a5a65a248" className="underline pl-1">Stephanie
+                            (Frankfurt)</a>
+                        <a href="https://www.linkedin.com/in/thorsten-thamm" className="underline pl-1">Thorsten Thamm
+                            (Köln)</a>,
                         Torben Lettschmidt
                     </p>
 
